e104ba8679a0219b7755a9d2f1178d408b26a549
Letícia Borges Ferreira <leticiabf90@gmail.com>
2025-03-20 18:14:00 -0400

    Add files via upload

---
+++ ex4.py
@@ -0,0 +1,6 @@
+num=int(input("inserir numero"))
+if num % 2 == 0:
+    print=input ("esse numero é par")
+else:
+    print=input ("esse numero é impar")
+    
---
+++ index.py
@@ -0,0 +1,65 @@
+#isso é um comentário.
+# print ("mensagem")
+# idade= 6
+# altura= 0.50
+# peso= 5.950
+# nome = "francisco"
+# print (idade, altura, peso, nome)
+# nome=input ("insira nome")
+# idade= int(input("insira idade"))
+# peso= float (input("inaira seu peso"))
+# print (f"{nome} {idade} anos {peso} kg") 
+# # f tem função de formatação
+
+# pedaco_pizza=5
+# cliente=( "john")
+# print (type(pedaco_pizza))
+# print (type ( cliente))
+# #string é o mesmo que caractere.
+# pedaco_pizza=input ("informe quantos pedaços comeu")
+# print(type(pedaco_pizza_))
+
+#PRINT MOSTRA PARA O USUÁRIO
+#type mostra o tipo da variavél
+#input coloca antes do texto # o ("f coloca para mostarr o valor de uma variavél para o usuário
+
+
+# A="Sally"
+# print (a)
+# print(A)
+
+# fruta1, fruta2, fruta3= "laranja", "banana","maça"
+#  print ( fruta1,fruta2, fruta3)
+
+# primeiro_numero=5
+# segundo_numero=10
+# print (primeiro_numero+ segundo_numero)
+
+#identação são os espaços, o numero de espaços recomendado 4 espaços, se colocar 4 espaços no
+# #  inicio, precisa fazer o mesmo no código todo, para não der erro.
+# nome= input ("Qual o vingador mais forte?") #input coloca antes do texto
+# print (f"Olá, {nome}!") #PRINT MOSTRA PARA O USUÁRIO
+# if nome == "hulk":  # dois = significa comparação #if será o nosso SE.
+#     print ("bem vindo vingador mqais forte!")#PRINT MOSTRA PARA O USUÁRIO
+# else:
+#     print("acesso negado!!!")
+# and é o mesmo que (e) retorna sverdade somente se ambas as condiçôes forem verdadeiras.
+
+# x=3
+# if x<2 or x>4: #apenas uma condição precisa ser verdadeira
+#   print ("o número é menor que 2 e maior que 4." )
+# # not não é lógico, inverte o valor lógico da condição, se for true, torna se false e vice versa.
+# x=5
+# if not x > 10: #inverte a condição (x não é maior que 10)
+#     print ("o número não é maior que 10.")
+
+# a ordem na uqal o python avalia os operadores lógicos é: 
+# 1 not
+# 2 and
+#3 or
+# x=5
+# y=8
+# if x >2 and (Y> 10 or not x==5 ):
+#         print("condição complexa atendida.")
+#  else:
+#         print("Condição não atendida.")
